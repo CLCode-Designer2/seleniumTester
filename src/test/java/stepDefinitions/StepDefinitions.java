5fe64e314b966b440cf150fec11debdcad036918
Calvin Hall <alcalgone@yahoo.com>
2021-03-18 10:39:31 +0100

    working addition test commit

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -5,47 +5,49 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class StepDefinitions {
 
     private WebDriver driver;
 
+
     @Given("I have entered {int} into the calculator")
     public void i_have_entered_70_into_the_calculator(Integer int1) throws InterruptedException {
         System.setProperty ("webdriver.chrome.driver", "chromedriver");
         driver = new ChromeDriver (); //Start Chrome
-        //driver.manage().window().maximize();
-        driver.get("https://www.marshu.com/articles/calculate-addition-calculator-add-two-numbers.php"); //Navigate to google.com
-        Thread.sleep(5000);  //Hold open for (5) seconds
-        WebElement input1 = driver.findElement (By.name ("n01"));
-        input1.sendKeys ("70");
+        driver.manage().window().maximize();
+        driver.get("https://www.marshu.com/articles/calculate-addition-calculator-add-two-numbers.php"); //Navigate web service
+        Thread.sleep(3000);  //Hold open for (3) seconds
+        WebElement input1 = driver.findElement (By.name ("n01")); //Find web element
+        input1.sendKeys (Integer.toString (int1)); //Input first number to element
     }
 
     @Given("I have also entered {int} into the calculator")
     public void i_have_also_entered_50_into_the_calculator(Integer int2) throws InterruptedException {
-        WebElement input2 = driver.findElement (By.name ("n02"));
-        input2.sendKeys ("50");
+        WebElement input2 = driver.findElement (By.name ("n02")); //Find web element
+        input2.sendKeys (Integer.toString (int2)); //Input second number to element
     }
 
     @When("I press add")
     public void i_press_add() throws InterruptedException {
         WebElement addButton = driver.findElement(By.xpath ("/html/body/table/tbody/tr/td[1]/table[2]/tbody/" +
-                "tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/form/p[4]/input"));
+                           "tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/form/p[4]/input"));
         addButton.click ();
     }
 
     @Then("the result should be {int} on the screen")
     public void the_result_should_be_120_on_the_screen(Integer result) throws InterruptedException {
         WebElement calcResult = driver.findElement (By.name ("answer"));
-        int actual = Integer.parseInt (calcResult.getAttribute ("value"));
-        int expected = result;
-        Assertions.assertEquals (expected, actual);
-        driver.close ();
+        int actual = Integer.parseInt (calcResult.getAttribute ("value")); //Assign result from web service to variable
+        int expected = result; //Assign result to a variable
+        Assertions.assertEquals (expected, actual); //Compare expected result to actual result
+        driver.close (); //Close web service
     }
-
 
 
 }
--- src/test/resources/Features/addition.feature
+++ src/test/resources/Features/addition.feature
@@ -13,4 +13,9 @@ Feature: Calc
     When I press add
     Then the result should be 579 on the screen
 
+  Scenario: (3) Add two numbers
+    Given I have entered 10456 into the calculator
+    And I have also entered 70890 into the calculator
+    When I press add
+    Then the result should be 81346 on the screen
 
